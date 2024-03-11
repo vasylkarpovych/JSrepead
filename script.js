12b8fe2ad7522ae2b91caea67335ccd892e6f847
Vasyl Karpovych <karpovichvasya@gmail.com>
2024-03-11 13:24:54 +0200

    mathematical, logical and conditional operators

--- script.js
+++ script.js
@@ -1,35 +1,202 @@
-// let a = "5";
-// let b = "13sdf";
-// let c = "12.9fdghhgf";
+//=====================================================================
+// Ввід/вивід данних
+// ====================================================================
 
-// console.log(Number(a));
-// console.log(parseInt(b));
-// console.log(parseFloat(c));
+// const message = "JavaScript is awesome!";
 
-// console.log(typeof Number(a));
-// console.log(typeof Number.parseInt(b));
-// console.log(typeof Number.parseFloat(c));
+// Введення
 
-// console.log((0.1 * 10 + 0.2 * 10) / 10);
+// alert(message);
+// console.log(message);
 
-// console.log(Math.max(12, 25, 46));
+// Отримання
 
-// console.log(Math.random() * (110 - 40) + 40);
+// confirm
+// const isConfirm = confirm("To be or not to be");
+// console.log(isConfirm);
 
-// console.log("Welcome to Bahamas".toLocaleUpperCase());
+// prompt
+// const userName = prompt("Please enter your name:");
+// console.log(userName);
 
-const user = {};
+//=====================================================================
+// Перетворення типу
+//=====================================================================
+// -----Number-----
+// const value = prompt("Please enter a number");
+// console.log(typeof parseInt(value));
+// console.log(Number(value));
+// const newValue = "123";
+// console.log(typeof +newValue);
 
-// console.log(user);
-user.name = "Goga";
-user.age = 19;
-user.city = "Tbilisy";
-// console.log(user);
-delete user.city;
-user["like flowers"] = true;
+// -----String-----
+// let value = false;
+// console.log(typeof value);
+// const newValue = String(value);
+// console.log(typeof newValue);
 
-// console.log(user);
+// -----Boolean-----
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(NaN));
+// console.log(Boolean(undefined));
+// console.log(Boolean("No"));
+// console.log(Boolean(""));
+// console.log(Boolean());
 
-for (key in user) {
-  console.log(`${key}: ${user[key]}`);
-}
+//=====================================================================
+// Оператори математичні
+//=====================================================================
+
+// Терміни: "унарний", "бінарний", "операнд"
+// Операнед - це те, до чого застосовуються оператори,
+// іноді їх назівають аргументами
+
+// let x = "45";
+// console.log(-x);
+// console.log(+x);
+// console.log(typeof x);
+// console.log(typeof -x);
+
+// Залишок віж ділення %
+// console.log(5 % 2); // у консолі 1
+// console.log(8 % 3); // у консолі 2
+// console.log(15 % 10); // у консолі 5
+// console.log(8 % 2); // у консолі 0
+
+// Піднесення до степеня **
+// console.log(5 ** 2); // у консолі 25
+// console.log(4 ** 4); // у консолі 25
+
+//=====================================================================
+// Оператори логічні (результатом повертають буль)
+//=====================================================================
+
+// a > b (a < b) // більше(менше)
+// a >= b (a <= b) // більше(менше) або дорівнює
+// a == b  // дорівнює
+// a != b  // не дорівнює
+// a === b  // строго дорівнює
+// a !== b  // строго не дорівнює
+
+// приведення до типу
+
+//=====================================================================
+// or -and -not
+//=====================================================================
+
+// ----- and (&&) -----
+// console.log(5 === 5 && 6 > 3); // true // запинається на брехні
+
+// ----- or (||) -----
+// console.log(5 !== 5 || 2 > 3); // false // запинається на правді
+// інакше false
+
+// ----- not (!) -----
+// console.log(!(5 === 5)); // false // міняє true на false
+// console.log(!false); // true // міняє false на true
+
+//=====================================================================
+// Умови is/else
+//=====================================================================
+
+// if (5 * 5 === 10) {
+//   console.log("Hello");
+// } else if (5 + 5 !== 10) {
+//   console.log("Done");
+// } else {
+//   console.log("Error");
+// }
+
+//=====================================================================
+// Тернарний оператор
+//=====================================================================
+
+// 5 * 5 === 10 ? console.log("Hello") : console.log("Error");
+
+//=====================================================================
+// switch/case
+//=====================================================================
+
+// let num = 9;
+
+// switch (num) {
+//   case 0:
+//     console.log("0");
+//     break;
+//   case 5:
+//     console.log("5");
+//     break;
+//   case 8:
+//     console.log("8");
+//     break;
+//   default:
+//     console.log("777");
+//     break;
+// }
+
+// let cost;
+// const subscribe = "premium";
+
+// switch (subscribe) {
+//   case "free":
+//     cost = 0;
+//   case "easy":
+//     cost = 20;
+//     break;
+//   case "standart":
+//     cost = 30;
+//     break;
+//   case "premium":
+//     cost = 50;
+//     break;
+//   default:
+//     console.log("Виникла помилка");
+// }
+
+// console.log(`Ваша підписка коштує ${cost}$`);
+
+//=====================================================================
+// Вирішення логічних задач і побудова алгоритмів
+//=====================================================================
+
+// let min = prompt("Введіть кількість хвилин від 0 до 60");
+
+// if (min <= 15) {
+//   console.log("Перша чверть години");
+// } else if (min <= 30) {
+//   console.log("Друга чверть години");
+// } else if (min <= 45) {
+//   console.log("Третя чверть години");
+// } else if (min <= 60) {
+//   console.log("Четверта чверть години");
+// } else {
+//   console.log("Введіть коректні дані");
+// }
+
+//=====================================================================
+
+// const value = prompt(
+//   "Введіть назву товару (яйце, молоко, хліб, піца)"
+// ).toLowerCase();
+// let cost;
+
+// switch (value) {
+//   case "яйце":
+//     cost = 10;
+//     alert(`${value} коштує ${cost} гривень`);
+//     break;
+//   case "молоко":
+//     cost = 50;
+//     alert(`${value} коштує ${cost} гривень`);
+//     break;
+//   case "хліб":
+//     cost = 25;
+//     alert(`${value} коштує ${cost} гривень`);
+//     break;
+//   case "піца":
+//     cost = 180;
+//     break;
+//   default:
+//     alert(`Такого товару ${value} немає в наявності або не існує`);
+// }
